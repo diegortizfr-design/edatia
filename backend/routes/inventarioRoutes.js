1e5fd92fd51ab417235e76245d08699168925bf3
desarrollos <diegortizfr@gmail.com>
2026-01-09 14:43:39 -0500

    fix: middleware import path typo

--- backend/routes/inventarioRoutes.js
+++ backend/routes/inventarioRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const inventarioController = require('../controllers/inventarioController');
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middlewares/authMiddleware');
 
 // Get Kardex for a product
 router.get('/kardex/:producto_id', protect, inventarioController.verKardex);
