abc7e6ff8f0946b6745d252a49b18853aff1e399
Diego Ortiz <tuemail@correo.com>
2026-01-31 14:12:05 -0500

    correciones

--- backend/routes/nominaRoutes.js
+++ backend/routes/nominaRoutes.js
@@ -2,11 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const nominaController = require('../controllers/nominaController');
-const auth = require('../middlewares/auth'); // Assuming there's an auth middleware
+const { protect } = require('../middlewares/authMiddleware');
 
-router.get('/cargos', auth, nominaController.getCargos);
-router.post('/cargos', auth, nominaController.createCargo);
-router.put('/cargos/:id', auth, nominaController.updateCargo);
-router.delete('/cargos/:id', auth, nominaController.deleteCargo);
+router.get('/cargos', protect, nominaController.getCargos);
+router.post('/cargos', protect, nominaController.createCargo);
+router.put('/cargos/:id', protect, nominaController.updateCargo);
+router.delete('/cargos/:id', protect, nominaController.deleteCargo);
 
 module.exports = router;
